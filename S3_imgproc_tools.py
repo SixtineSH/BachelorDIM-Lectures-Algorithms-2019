fe76cb4759185fe185b85da9f62a7a0821051a8f
Sixtine SCHUHLER-HUSSON <schuhles@iut-acy.local>
2019-09-30 15:49:38 +0200

    inverted image

--- S3_imgproc_tools.py
+++ S3_imgproc_tools.py
@@ -8,7 +8,7 @@ Created on Mon Sep 30 13:48:32 2019
 import cv2
 import numpy as np
 
-img_gray=cv2.imread('python.png',0)
+'''img_gray=cv2.imread('python.png',0)
 img_bgr=cv2.imread('python.png',1)
 
 print("Gray levels image shape = "+str(img_gray.shape))
@@ -16,4 +16,19 @@ print("BGR image shape = "+str(img_bgr.shape))
 
 cv2.imshow("Gray levels image", img_gray)
 cv2.imshow("BGR image", img_bgr)
+cv2.waitKey()'''
+
+img=cv2.imread('rick.jpg')
+
+cv2.imshow('input', img)
+cv2.waitKey()
+
+'''for row in range(img.shape[0]):
+    for col in range(img.shape[1]):
+        for cha in range(img.shape[2]):
+            img[row, col, cha] = 255-img[row, col,cha]'''
+
+img=255-img
+            
+cv2.imshow('inverted input', img)
 cv2.waitKey()
