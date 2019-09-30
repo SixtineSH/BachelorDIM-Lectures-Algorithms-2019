e0698ade3644a8fadb1bf36567ef58cb7e0a635c
Sixtine SCHUHLER-HUSSON <schuhles@iut-acy.local>
2019-09-30 11:57:47 +0200

    4 tests function max value

--- test_S2.py
+++ test_S2.py
@@ -32,3 +32,24 @@ def test_average_above_zero_non_numbers():
     tab_list=['a', 'ef', 'dzv', 'cdz', 'dc']
     with pytest.raises(ValueError):
         tobetested.average_above_zero(tab_list)
+
+
+def test_function_max_value_working1():
+    tab_list=[1,2,3,-5,6,-9]
+    test=tobetested.function_max_value(tab_list)
+    assert test==(6,4)
+
+def test_function_max_value_empty_table():
+    tab_list=[]
+    with pytest.raises(ValueError):
+        tobetested.function_max_value(tab_list)
+        
+def test_function_max_value_no_list():
+    tab_list=250
+    with pytest.raises(ValueError):
+        tobetested.function_max_value(tab_list)
+        
+def test_function_max_value_non_numbers():
+    tab_list=['12', '15', '125', '512', '2048']
+    with pytest.raises(ValueError):
+        tobetested.function_max_value(tab_list)
