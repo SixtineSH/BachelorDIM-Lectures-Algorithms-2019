a07702b685b76e3b34dc0f3535b0b6cdba7bd1e3
Sixtine SCHUHLER-HUSSON <schuhles@iut-acy.local>
2019-09-30 14:19:29 +0200

    test sonarcloud 2

--- S3_imgproc_tools.py
+++ S3_imgproc_tools.py
@@ -5,3 +5,15 @@ Created on Mon Sep 30 13:48:32 2019
 @author: schuhles
 """
 
+import cv2
+import numpy as np
+
+img_gray=cv2.imread('python.png',0)
+img_bgr=cv2.imread('python.png',1)
+
+print("Gray levels image shape = "+str(img_gray.shape))
+print("BGR image shape = "+str(img_bgr.shape))
+
+cv2.imshow("Gray levels image", img_gray)
+cv2.imshow("BGR image", img_bgr)
+cv2.waitKey()
