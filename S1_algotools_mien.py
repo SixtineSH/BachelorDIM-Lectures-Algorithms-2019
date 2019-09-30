4d678a4aca2ba5510b2b273dca9257467baa4eb2
Sixtine SCHUHLER-HUSSON <schuhles@iut-acy.local>
2019-09-30 10:57:54 +0200

    3rd try

--- S1_algotools_mien.py
+++ S1_algotools_mien.py
@@ -15,14 +15,14 @@ import numpy as np
 tab_zeros= np.zeros(12, dtype=np.int32)
 tab_from_list=np.array(tab_list)
 
-for id in range(len(tab_from_list)):
+'''for id in range(len(tab_from_list)):
     print('tab['+str(id)+']='+str(tab_from_list[id]))
     print('tab[{index}]={val}'.format(index=id, val=tab_from_list[id]))
     
     if tab_from_list[id]>0:
         print('Youpi')
 
-print('Finished')
+print('Finished')'''
 
 def mySum(param1, param2):
     '''
@@ -60,6 +60,11 @@ def average_above_zero(table):
         raise ValueError('Division by 0')    
     return som / n
 print('Moyenne = ', average_above_zero(tab_list))
+
+'''#test section
+tab_list=[1,2,3,4,5,6,-9]
+test, lasID=average_above_zero(tab_list)
+print('test_sum=', test)'''
 
 '''
 MAX VALUE 
@@ -132,7 +137,7 @@ for i <- 1 to NMAX do
         
 '''
 
-import numpy as np
+'''import numpy as np
 matrix = np.zeros((10,10), dtype=np.int32)
 
 matrix[3:6, 4:8]=np.ones((3,4), dtype=np.int32)
@@ -144,8 +149,30 @@ cv2.waitKey()
 
 for idrow in range(matrix.shape[0]):
     for idcol in range(matrix.shape[1]):
-        pixVal=matrix[idrow, idcol]
+        pixVal=matrix[idrow, idcol]'''
         
 #x,y,w,h = cv2.boundingRect(cnt) for x,y top-left coordinates and w,h bottom-right coordinates
 #img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+            
+'''top = H
+bottom = 0
+left = 
+right = 
+for col in range (L):
+    for row in range (H):
+        if mat[row,col] == 1:
+            if top > row:
+                top = row
+            if bottom < row:
+                bottom = row
+            if left < col:
+                left = col
+            if right > col:
+                right = col
+        if top == -1: #4 boucles en parallèle
+            top = row
+            break
+        if top != -1:
+            break'''
+        
             
