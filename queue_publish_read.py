fc20437e23e52128dedae6095a92851c50646809
Sixtine SCHUHLER-HUSSON <schuhles@iut-acy.local>
2019-10-01 10:55:42 +0200

    queue publish read and counter

--- queue_publish_read.py
+++ queue_publish_read.py
@@ -19,21 +19,32 @@ parser.add_argument("--read", help="read the script",
                     action="store_true")
 args = parser.parse_args()
 
+cpt = 0 #to know th numbers of received messages
+
 if args.read==True:
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
     channel.queue_declare(queue='presentation')
+    cpt = 0
 
     def callback(ch, method, properties, body):
+            ##
+            #Function that receive and print messages from the queue
+            #Returns : The received messages and the count
         print(" [x] Received %r" % body)
+        global cpt
+        cpt += 1
+        print('Count = %d' %cpt)        
 
     channel.basic_consume(queue='presentation',
                           on_message_callback=callback,                          
                           auto_ack=True)
+
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
     
 else:
+    #Send messages to the queue
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
     channel.queue_declare(queue='presentation')
