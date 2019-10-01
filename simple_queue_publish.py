9bde069a663e4361189c3e7241fcafb8eeb82d22
Sixtine SCHUHLER-HUSSON <schuhles@iut-acy.local>
2019-10-01 12:00:35 +0200

    end of session multiple readers

--- simple_queue_publish.py
+++ simple_queue_publish.py
@@ -7,6 +7,7 @@ Created on Tue Oct  1 08:44:11 2019
 
 import os
 import pika
+import argparse
 
 amqp_url='amqp://oyuxrrne:gfgMfJSFtXMWivHwAfRzsKEp-ZAL0dMS@dove.rmq.cloudamqp.com/oyuxrrne'
 
@@ -14,14 +15,36 @@ url = os.environ.get('CLOUDAMQP_URL',amqp_url)
 params = pika.URLParameters(url)
 params.socket_timeout = 5
 
-connection = pika.BlockingConnection(params)
-channel = connection.channel()
-channel.queue_declare(queue='presentation')
 
-channel.basic_publish(exchange='',
-    routing_key='presentation',
-    body='Sixtine Schuhler-Husson')
-                          
-print(" [x] Sent 'Sixtine Schuhler-Husson'")
+parser = argparse.ArgumentParser()
+parser.add_argument("--concurrency", help="send to multiple queues",
+                    action="store_true")
+args = parser.parse_args()
+i = 0
+
+if args.concurrency==True:
+    while i < 1000:
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+        channel.queue_declare(queue='presentation')
+
+        channel.basic_publish(exchange='',
+                              routing_key='presentation',
+                              body='Sixtine Schuhler-Husson',
+                              properties=pika.BasicProperties(
+                                      delivery_mode = 2))                      
+        print(" [x] Sent 'Sixtine Schuhler-Husson'")
+        connection.close()
+        i += 1
     
-connection.close()
+else:
+    #Send messages to the queue
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
+    channel.queue_declare(queue='presentation')
+
+    channel.basic_publish(exchange='',
+                          routing_key='presentation',
+                          body='Sixtine Schuhler-Husson')
+    print(" [x] Sent 'Sixtine Schuhler-Husson'")
+    connection.close()
--- simple_queue_read.py
+++ simple_queue_read.py
@@ -7,6 +7,7 @@ Created on Tue Oct  1 08:55:23 2019
 
 import os
 import pika
+import argparse
 
 amqp_url='amqp://oyuxrrne:gfgMfJSFtXMWivHwAfRzsKEp-ZAL0dMS@dove.rmq.cloudamqp.com/oyuxrrne'
 
@@ -14,16 +15,39 @@ url = os.environ.get('CLOUDAMQP_URL',amqp_url)
 params = pika.URLParameters(url)
 params.socket_timeout = 5
 
-connection = pika.BlockingConnection(params)
-channel = connection.channel()
-channel.queue_declare(queue='presentation')
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+parser = argparse.ArgumentParser()
+parser.add_argument("--concurrency", help="send to multiple queues",
+                    action="store_true")
+args = parser.parse_args()
 
-channel.basic_consume(queue='presentation',
-     on_message_callback=callback,                          
-     auto_ack=True)
+if args.concurrency==True:
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
+    channel.queue_declare(queue='presentation')
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        print(" [x] Message Processed, acknowledging (to delete message from the queue)")
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+
+    channel.basic_consume(queue='presentation',
+                          on_message_callback=callback,                          
+                          auto_ack=True)
     
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+    channel.start_consuming()
+    
+else:
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
+    channel.queue_declare(queue='presentation')
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+
+    channel.basic_consume(queue='presentation',
+                          on_message_callback=callback,                          
+                          auto_ack=True)
+    
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
